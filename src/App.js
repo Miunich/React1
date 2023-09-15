f5e5dba4347addc1921352b056d707308f5c2042
Sergio Espinoza <sergio.es.her@gmail.com>
2023-09-15 15:45:19 -0300

    React aprendizaje 01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import imagen from "./Pableñe.jpg";
+import './MiCss.css';
 
 function App() {
+  // let nombre=["Jab","Pepito","Guau"] //arreglo
+  let nombre={name:"jab",edad:18}
+  // let caja="<div>Nombre</div>"
+  let caja=<div>Nombre</div>
+  let nombres=["Pableñe","Pablo","Pablito"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fondo">
+      {/* <h1>{nombre[1]}</h1>//arreglo */}
+      <h1>{nombre.edad}</h1>
+      {/* <input type="text"/> */}
+      <br></br>
+      <img src={imagen} className="im"/>
+      {/* {alert("Hola Mundo")} */}
+      <input value={nombre.name}></input>
+      <div>{caja}</div>
+      <div className="contenedor">
+        <div className="caja">
+          <img src={imagen} className="im"/>
+          <div className="nombre">{nombres[0]}</div>
+
+        </div>
+        <div className="caja">
+          <img src={imagen}className="im"/>
+          <div className="nombre">{nombres[1]}</div>
+
+        </div>
+        <div className="caja">
+          <img src={imagen}className="im"/>
+          <div className="nombre">{nombres[2]}</div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
